01c5b979560c8911459ed023de0eb369d3b87178
Christopher Dopona Lopes <chrisdopona@outlook.com>
2018-06-17 16:18:21 -0300

    Utilizando como exemplo use strict, função IIFE, e arguments.

---
+++ funcoes/problema/index.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+    function calcularMedia() {
+        var total = 0;
+        var qtd = arguments.length;
+        var x = 0;
+        
+        while(typeof arguments[x] === 'number'){
+            total += arguments[x];
+            x++;
+        }
+        return total / qtd;
+    }
+    
+    var mediaUm = calcularMedia(2, 3);
+    var mediaDois = calcularMedia(4, 0, 10, 10);
+
+    console.log("Media um: ", mediaUm);
+    console.log("Media Dois: ", mediaDois);
+    
+})();
