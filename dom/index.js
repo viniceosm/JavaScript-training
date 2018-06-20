a20eb06415868e194ddfa4464268fbbd331f7317
Christopher Dopona Lopes <chrisdopona@outlook.com>
2018-06-19 21:41:13 -0300

    Organiza arquivo e faz um exemplo.

--- dom/index.js
+++ dom/index.js
@@ -1,28 +1,31 @@
-/**
- * OBJETO DOCUMENT
- * getElementById
- * getElementsByTagName
- * getElementsByClassName
- * querySelector
- * querySelectorAll
- */
+(function () {
+    /**
+     OBJETO DOCUMENT
 
-// document no console, mostra todo o html.
+        getElementById
+            Atribuindo um novo conteudo ao paragrafo.
+            document.getElementById('pmain').textContent = 'Texto alterado';
 
-// Atribuindo um novo conteudo ao paragrafo.
-document.getElementById('pmain').textContent = 'Texto alterado';
+        getElementsByTagName
+            Busca valores referente a tag, sendo assim, tudo que está dentro da tag.
+            document.getElementsByTagName('p');
 
-// Busca valores referente a tag, sendo assim, tudo que está dentro da tag.
+        getElementsByClassName
+            Alterando texto de um getElementByClassName, é obrigado a passar o indice.
+            document.getElementsByClassName('paragrafos')[0].textContent = 'texto alterado';
 
-document.getElementsByTagName('p');
+        querySelector
+            querySelector sempre vai retornar o primeiro elemento
+            document.querySelector("#idmain") 
+            document.querySelector("#idmain p") // pegando o paragrafo
+            document.querySelector("#idmain h2") // pegando o h2
+            document.querySelector("#idmain h2").textContent = 'Alterado' // alterando valor
 
-// Alterando texto de um getElementByClassName, é obrigado a passar o indice.
-document.getElementsByClassName('paragrafos')[0].textContent = 'texto alterado';
+        querySelectorAll
+            querySelectorAll sempre vai retornar uma coleção de objetos mesmo que seja de um unico elemento
 
-// querySelector sempre vai retornar o primeiro elemento
-document.querySelector("#idmain") 
-document.querySelector("#idmain p") // pegando o paragrafo
-document.querySelector("#idmain h2") // pegando o h2
-document.querySelector("#idmain h2").textContent = 'Alterado' // alterando valor
+        Ao digitar document no console, mostra todo o html.
+    */
 
-// querySelectorAll sempre vai retornar uma coleção de objetos mesmo que seja de um unico elemento
+    document.querySelector('h1').textContent = 'Paragrafo alterado via JavaScript';
+})();
