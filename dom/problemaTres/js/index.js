bf142c805ad405f4b3d1f946b3d9b2be2301429c
Christopher Dopona Lopes <chrisdopona@outlook.com>
2018-06-20 21:44:59 -0300

    Refatoração do código JS, uitilizando apply para calcular a media.

--- dom/problemaTres/js/index.js
+++ dom/problemaTres/js/index.js
@@ -4,15 +4,24 @@
 
     while(trs[x]){
         var linhaAluno = trs[x];
-        var n1, n2, n3, n4, media;
         var tds = linhaAluno.querySelectorAll('td');
-        n1 = pegaNota(tds[1]); n2 = pegaNota(tds[2]);  n3 = pegaNota(tds[3]); n4 = pegaNota(tds[4]);
-        media = calcularMedia(n1, n2, n3, n4);
+        var notas = recuperarNumerosDeColunas(tds);
+
+        media = calcularMedia.apply(this, notas);
+
         linhaAluno.querySelector('.aluno-m').textContent = media;
         x++;
     };
 
-    function pegaNota(td) {
-        return parseFloat(td.textContent);
-    };   
+    function recuperarNumerosDeColunas(tds) {
+      var x = 0;
+      var arr = [];
+      while (tds[x]) {
+        if (!isNaN(tds[x].textContent) && tds[x].textContent != '') {
+            arr.push(parseFloat(tds[x].textContent));
+        };
+        x++;
+      };
+      return arr;
+    };
 })();
