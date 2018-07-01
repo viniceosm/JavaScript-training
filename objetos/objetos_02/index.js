0cc5200b9ecd765a4bd5faa8ef6ea40f06162dcd
Christopher Dopona Lopes <chrisdopona@outlook.com>
2018-06-30 23:14:25 -0300

    Modificações na chamada da função para estado global.

--- objetos/objetos_02/index.js
+++ objetos/objetos_02/index.js
@@ -1,14 +1,15 @@
-function Caneta(preco, cor) {
-
-    var color =  cor || 'black';
-    this.preco = preco || 1;
-    this.mudarCor = function (cor) {
-        color = cor;
-    };  
-
-    this.pegaCor = function(){
-        return color;
+var Caneta = (function (window) {
+    'use strict';
+    function _caneta(preco, cor) {
+        var color =  cor || 'black';
+        this.preco = preco || 1;
+        this.mudarCor = function (cor) {
+            color = cor;
+        };  
+        this.pegaCor = function(){
+            return color;
+        }
     }
-}
 
-var caneta1 = new Caneta(200, 'blue');
+    return _caneta;
+})(window)
