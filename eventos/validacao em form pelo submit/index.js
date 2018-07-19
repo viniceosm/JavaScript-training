9ef3a64f97460a6200f4aeb3e4c5280860fdb6ef
Christopher Dopona Lopes <chrisdopona@outlook.com>
2018-07-19 19:39:18 -0300

    Validação em form de login usando submit.

--- eventos/validacao em form pelo submit/index.js
+++ eventos/validacao em form pelo submit/index.js
@@ -1,11 +1,14 @@
 (function () {
     'use strict';
+    var $form = document.querySelector('form');
     var $txtUser = document.getElementById('txtUser');
     var $txtPass = document.getElementById('txtPass');
     var $btn = document.getElementById('btn');
 
-    $btn.addEventListener('click', () => {
-        if (!$txtUser.value || !$txtPass.value)
+    $form.addEventListener('submit', (e) => {
+        if (!$txtUser.value || !$txtPass.value) {
             alert('Preencha todos os campos')
+            e.preventDefault();
+        };
     });
 })();
