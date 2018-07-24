e306317d63fc100fb94c16678ed88241fab2a5e9
Christopher Dopona Lopes <chrisdopona@outlook.com>
2018-07-24 19:28:00 -0300

    Utilizando setAttribute e getAttribute.

--- eventos/desafio/javascript/menu.js
+++ eventos/desafio/javascript/menu.js
@@ -1,11 +1,18 @@
-(function () {    
-    var html = document.querySelector('html');
-    var btn = document.querySelector('.header-nav__hamburgger');
+(function(){
 
-    html.classList.remove('no-js');
-    html.classList.add('js');
+    var $html = document.querySelector('html');
+    var $btn = document.querySelector('.header-nav__hamburgger');
+    var arialControl = $btn.getAttribute('aria-controls');
+    var $menu = document.getElementById(arialControl);
+    
+    $html.classList.remove('no-js');
+    $html.classList.add('js');
+    
+    $btn.addEventListener('click', function(){
+        $html.classList.toggle('menu-opened');
+        var ariaExpanded = this.getAttribute('aria-expanded') === "true";
 
-    btn.addEventListener('click', function() {
-        html.classList.toggle('menu-opened');
-    });
-})
+        $btn.setAttribute('aria-expanded', !ariaExpanded);
+        $menu.setAttribute('aria-expanded', !ariaExpanded);
+    })
+})()
