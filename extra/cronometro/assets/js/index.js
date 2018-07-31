2c0d47cb3361a21d223e989c65341d6d66beda8a
Christopher Dopona Lopes <chrisdopona@outlook.com>
2018-07-31 13:32:05 -0300

    Organiza código.

--- extra/cronometro/assets/js/index.js
+++ extra/cronometro/assets/js/index.js
@@ -30,10 +30,9 @@ function iniciarCronometro() {
 	btnIniciar.disabled = true; btnLimpar.disabled = false; btnParar.disabled = false;
 };
 
-function informarHoraAtual() {
+function infoHoraDataAtual() {
 	const nomeDias = new Array("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado");
 	const nomeMes = new Array("janeiro", "fevereiro", "março", "abril", "maio", "junho", "Julho", "agosto", "setembro", "outubro", "novembro", "dezembro");
-
 	let dataHoraAtual = new Date();
 	let dia = dataHoraAtual.getDate(); // 1-31
 	let diaSem = dataHoraAtual.getDay(); // 0-6 (zero=domingo)
@@ -43,7 +42,6 @@ function informarHoraAtual() {
 	let min = dataHoraAtual.getMinutes(); // 0-59
 	let seg = dataHoraAtual.getSeconds(); // 0-59
 
-
 	hora = hora.toString().padStart(2, '0');
 	min = min.toString().padStart(2, '0');
 	seg = seg.toString().padStart(2, '0');
@@ -51,22 +49,20 @@ function informarHoraAtual() {
 	infoHoraAtual.innerHTML = `${hora}:${min}:${seg}`;
 	infoDataAtual.innerHTML = `${nomeDias[diaSem]}, ${dia} de ${nomeMes[mes]} de ${ano}`;
 
-	setTimeout("informarHoraAtual()", 1000);
+	setTimeout("infoHoraDataAtual()", 1000);
 };
 
 btnIniciar.addEventListener('click', iniciarCronometro);
 
 btnParar.addEventListener('click', () => {
 	pararCronometro();
-
 	btnIniciar.disabled = false; btnLimpar.disabled = false; btnParar.disabled = true;
 });
 
 btnLimpar.addEventListener('click', () => {
 	pararCronometro();
 	limparCronometro();
-
 	btnIniciar.disabled = false; btnLimpar.disabled = true; btnParar.disabled = true;
 });
 
-informarHoraAtual();
+infoHoraDataAtual();
