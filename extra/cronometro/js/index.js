c230d95c210a71b60bf3754dfaff06d1bcefbf88
Christopher Dopona Lopes <chrisdopona@outlook.com>
2018-07-31 11:20:54 -0300

    Melhorias no código, adiciona botões parar e limpar.

--- extra/cronometro/js/index.js
+++ extra/cronometro/js/index.js
@@ -1,18 +1,40 @@
 const btnIniciar = document.getElementById('btnIniciar');
+const btnParar = document.getElementById('btnParar');
+const btnLimpar = document.getElementById('btnLimpar');
+let s = 1; m = 0; h = 0; iniciar = null;
 
-let s = 1; m = 0; h = 0;
+function pararCronometro() {
+	clearInterval(iniciar);
+};
 
-btnIniciar.addEventListener('click', () => {
-	window.setInterval(function () {
-		s == 60 ? (m++, s = 0) : '';
-		m == 60 ? (h++, s = 0, m = 0) : '';
+function iniciarCronometro() {
+	iniciar = window.setInterval(() => {
+		s == 60 ? (m++ , s = 0) : '';
+		m == 60 ? (h++ , s = 0, m = 0) : '';
 
 		h < 10 ? spanHora.textContent = `0${h}` : spanHora.textContent = `${h}`;
 		m < 10 ? spanMinuto.textContent = `0${m}` : spanMinuto.textContent = `${m}`;
 
-		/* If substituido por padStart
-		s < 10 ? spanSegundo.textContent = `0${s}` : spanSegundo.textContent = `${s}`; */
 		spanSegundo.textContent = s.toString().padStart(2, '0');
 		s++;
+
 	}, 1000);
+
+	btnIniciar.disabled = true; btnLimpar.disabled = false; btnParar.disabled = false;
+};
+
+btnIniciar.addEventListener('click', iniciarCronometro);
+
+btnParar.addEventListener('click', () => {
+	pararCronometro();
+
+	btnIniciar.disabled = false; btnLimpar.disabled = false; btnParar.disabled = true;
+});
+
+btnLimpar.addEventListener('click', () => {
+	pararCronometro();
+
+	spanHora.textContent = '00'; spanMinuto.textContent = '00'; spanSegundo.textContent = '00';
+
+	btnIniciar.disabled = false; btnLimpar.disabled = true; btnParar.disabled = true;
 });
